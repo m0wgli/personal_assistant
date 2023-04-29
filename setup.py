28d66bb1918fb3d5a79af1de71db3280a3734c9b
m0wgli <rodiontiutiunyk@gmail.com>
2023-04-30 01:29:11 +0300

    setup.py and __init__.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_namespace_packages
+
+setup(name='personal_assistant',
+      version='0.0.1',
+      author='Rodion Tiutiunyk, Ihor Moroz, Vladimir Grebeniuk, Andrii Siryi',
+      license='MIT',
+      packages=find_namespace_packages(),
+      entry_points={'console_scripts': ['personal_assistant = personal_assistant.main:main']})
